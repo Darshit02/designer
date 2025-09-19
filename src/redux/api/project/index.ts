d14e0592c13edf65599a7901c7dfbadde3990feb
Darshit02 <darshitmistry2@gmail.com>
2025-09-19 18:53:21 +0530

    feat : navbar and redux

---
+++ src/redux/api/project/index.ts
@@ -0,0 +1,45 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+type AutoSavePojectResponse = {
+  success: boolean;
+  message: string;
+  eventId: string;
+};
+
+type AutoSavePojectRequest = {
+  projectId: string;
+  userId: string;
+  shapesData: {
+    shapes: Record<string, unknown>;
+    tools: string;
+    selected: Record<string, unknown>;
+    freamCounter: number;
+  };
+  viewPortData?: {
+    scale: number;
+    translate: {
+      x: number;
+      y: number;
+    };
+  };
+};
+
+export const ProjectApi = createApi({
+  reducerPath: "projectApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "/api/project",
+  }),
+  tagTypes: ["Project"],
+  endpoints: (builder) => ({
+    autosaveProject: builder.mutation<
+      AutoSavePojectResponse,
+      AutoSavePojectRequest
+    >({
+      query: (data) => ({
+        url : '' ,
+        method : 'PATCH',
+        body : data
+      }),
+    }),
+  }),
+});
